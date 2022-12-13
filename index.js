a7431b1d58f358562f3ea4ffa16f23b89c87921e
Galen Beck Gatzke <gbgatzke@gmail.com>
2022-12-13 10:09:04 -0700

    commit tree 8e3459ddd7f6103b7022c501fc617d6508284778

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+ function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+ }
+
+ function appendCat(name) {
+    const copyCats = [
+        ...cats, "Broom"
+    ]
+    return copyCats
+ }
+
+ function prependCat(name) {
+    const copyCats = [
+        "Arnold", ...cats
+    ]
+    return copyCats
+ }
+  function removeLastCat(name) {
+    const copyCats = cats.slice(0, 2)
+    
+    return copyCats
+  }
+
+  function removeFirstCat(name) {
+    const copyCats = cats.slice(-2)
+    return copyCats
+  }
